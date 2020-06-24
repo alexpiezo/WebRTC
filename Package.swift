606d954671d1f17d15992f4e013731c23538f534
Alex <alex@piezo.my>
2020-06-25 01:21:21 +0800

    fix binaryTarget url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "WebRTC",
-                      url: "https://github.com/alexpiezo/WebRTC/releases/download/1.1.29507/WebRTC-1.0.29507.xcframework.zip",
+                      url: "https://github.com/alexpiezo/WebRTC/releases/download/1.1.29507/WebRTC.xcframework.zip",
                       checksum: "094e669df5ba17aa423630ea785706a6a5cfaacbf426703bdd793703f4807ff0")
         
     ]
