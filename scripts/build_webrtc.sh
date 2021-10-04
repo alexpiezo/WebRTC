5fa33247e40f4bd22a765962af92d95f98a84192
Alex <alex@piezo.my>
2021-10-05 01:29:31 +0800

    fix build script

--- scripts/build_webrtc.sh
+++ scripts/build_webrtc.sh
@@ -11,10 +11,12 @@ is_debug=${IS_DEBUG:-false} \
 is_component_build=${IS_COMPONENT_BUILD:-false} \
 rtc_include_tests=${RTC_INCLUDE_TESTS:-false} \
 rtc_libvpx_build_vp9=${RTC_LIBVPX_BUILD_VP9:-true} \
-rtc_enable_symbol_export=${RTC_ENABLE_SYMBOL_EXPORT:-true} \
+rtc_enable_objc_symbol_export=${RTC_ENABLE_SYMBOL_EXPORT:-true} \
 rtc_enable_protobuf=${RTC_ENABLE_PROTOBUF:-false} \
+rtc_include_tests=false \
 treat_warnings_as_errors=${TREAT_WARNINGS_AS_ERRORS:-false} \
-enable_stripping=${ENABLE_STRIPPING:-true}"
+enable_stripping=${ENABLE_STRIPPING:-true} \
+enable_dsyms=false"
 
 if [[ $TARGET_OS = "ios" ]]
 then
@@ -25,7 +27,8 @@ then
     ios_enable_code_signing=${IOS_ENABLE_CODE_SIGNING:-false} \
     enable_ios_bitcode=${ENABLE_IOS_BITCODE:-false} \
     use_xcode_clang=${USE_XCODE_CLANG:-true} \
-    use_goma=${USE_GOMA:-false}"
+    use_goma=${USE_GOMA:-false}\
+    "
 
     echo "${C_GREEN}${C_GREY0}Build ${TARGET_OS} ${TARGET_CPU}${NO_FORMAT} with args ${C_AQUA}${C_GREY0}${gen_args}${NO_FORMAT}"
 
